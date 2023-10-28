f513dbbb6a27f0307ec6fe47f16e6d0cce68dcb8
RaunakShrivastwa <kumarraunak4201@gmail.com>
2023-10-28 11:20:56 -0700

    commite

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import router from './router/HomeRouter.js';
 import db from './config/Db.js';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
+import { pid } from 'process';
 
 if (cluster.isPrimary) {
     for (let i = 0; i < os.cpus().length; i++) {
@@ -15,16 +16,16 @@ if (cluster.isPrimary) {
     dotenv.config();
     const PORT = process.env.PORT;
     const app = express();
-     app.use(cors('*'))
-     app.use(fileUpload({
-        useTempFiles:true
-     }))
+    app.use(cors('*'))
+    app.use(fileUpload({
+        useTempFiles: true
+    }))
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     // use Router to handle All HTTP
     app.use('/', router)
     app.listen(PORT, () => {
-        console.log('Server is Running at Port', PORT)
+        console.log(`Server is running at port ${PORT} with process ID ${pid}`)
     });
     app.on('error', (err) => {
         console.log("There is error ", err);
