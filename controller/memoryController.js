5cbf49d2fc6a362442e93cc6681828146d84fcbe
shu2002k <shubhkuttiya@gmail.com>
2023-10-28 03:15:21 -0700

    commite

--- controller/memoryController.js
+++ controller/memoryController.js
@@ -1,7 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary';
+import User from '../model/User.js';
+import Memory from '../model/Memory.js'
+cloudinary.config({
+    cloud_name: 'dzhl7dmsp',
+    api_key: '323466687184448',
+    api_secret: 't8YR62TWigv3q2gTHL3mtIoTVzM'
+});
 export default class Memory{
 
     addMemory= async (req,res)=>{
-
+        const userId= req.params.id;
+        try{
+             const img= req.files.img;
+            const upload= await cloudinary.uploader.upload(img.tempFilePath)
+            
+        }catch(err){
+            console.log("There is Error With Operations ",err);
+            return;
+        }
     }
 
     getAllMemory = async (req,res)=>{
