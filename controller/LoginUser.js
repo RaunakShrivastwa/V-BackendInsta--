f5cd73763891d816f3e75641cbec5c2428114347
Ks-Shubham <Connect2Shubham@outlook.com>
2023-11-30 21:01:43 +0530

    commit

--- controller/LoginUser.js
+++ controller/LoginUser.js
@@ -1,7 +1,11 @@
+import User from "../model/User.js"
+
 export default class AuthenticationClass {
 
-    loginuser = (req, res) => {
-        return res.json({ Message: 'User Login Successfully!!!!' })
+    loginuser = async (req, res) => {
+        const user = await User.findOne({ email: req.body.email })
+        console.log(req.body);
+        return res.json(user)
     }
 
     logout = (req, res) => {
